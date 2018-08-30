d0ed004f4e3d3a11a1a4abb3187f9bec2b52a4f2
Tom Wolfe <TOM356@bailliegifford.com>
2018-08-30 16:46:21 +0100

    Get the beginning currency stuff working.

---
+++ src/intents/currency.intent.ts
@@ -0,0 +1,32 @@
+import { HandlerInput } from 'ask-sdk-core';
+import { Intent, Response } from 'ask-sdk-model';
+
+import * as Currencies from '../data/currencies.json';
+import { IntentHandler, IntentMap } from '../framework';
+import * as Utils from '../utils';
+
+export class CurrencyIntentHandler extends IntentHandler {
+  get intents(): IntentMap {
+    return {
+      CurrencyConversionIntent: this.currencyConversion,
+    };
+  }
+
+  currencyConversion(handlerInput: HandlerInput, intent: Intent): Response {
+    const value = Number(Utils.slotValue(intent.slots.value));
+    const from = Utils.slotValue(intent.slots.value);
+    const to = Utils.slotValue(intent.slots.value);
+
+    let speech = '';
+    if (to) {
+      const exchangeRate = Currencies[from][to];
+      speech = `${value * exchangeRate} ${to} pieces`;
+    } else {
+      // TODO: Get this part working
+    }
+
+    return handlerInput.responseBuilder
+      .speak(speech)
+      .getResponse();
+  }
+}
