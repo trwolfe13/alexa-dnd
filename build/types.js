cb4db7dc253621346639d3701cf067fdfe24f843
Tom Wolfe <trwolfe13@gmail.com>
2019-05-14 21:16:18 +0100

    Drastically simplify the intents

--- build/types.js
+++ build/types.js
@@ -11,29 +11,18 @@ const defaultMap = obj => {
   return value;
 }
 
-const addSynonyms = synonyms => obj => {
-  if (!synonyms || synonyms.length === 0) { return obj; }
-  if (!obj.synonyms) { obj.synonyms = []; }
-  obj.synonyms.push(...synonyms.map(s => s.replace('{0}', obj.name.toLowerCase())));
-  return obj;
-}
-
-const createType = (dir, file, name, synonyms) => {
+const createType = (dir, file, name) => {
   const type = { name, values: [] };
   const list = JSON.parse(fs.readFileSync(path.join(dir, file)));
-  type.values = list.map(addSynonyms(synonyms)).map(defaultMap);
+  type.values = list.map(defaultMap);
   return type;
 };
 
 const createSubclass = dir => {
   const type = { name: 'Subclass', values: [] };
   const list = JSON.parse(fs.readFileSync(path.join(dir, 'classes.json')));
-  const synonyms = addSynonyms([
-    'the {0}',
-    'the {0} subclass',
-  ]);
   list.forEach(characterClass => {
-    type.values.push(...characterClass.subclasses.map(synonyms).map(defaultMap));
+    type.values.push(...characterClass.subclasses.map(defaultMap));
   });
   return type;
 };
@@ -41,26 +30,21 @@ const createSubclass = dir => {
 const createSubrace = dir => {
   const type = { name: 'Subrace', values: [] };
   const list = JSON.parse(fs.readFileSync(path.join(dir, 'races.json')));
-  const synonyms = addSynonyms([
-    'the {0}',
-    'the {0} race',
-    'the {0} subrace',
-  ]);
   list.forEach(race => {
-    type.values.push(...race.subraces.map(synonyms).map(defaultMap));
+    type.values.push(...race.subraces.map(defaultMap));
   });
   return type;
 };
 
 module.exports = {
-  createSpell: dir => createType(dir, 'spells.json', 'Spell', ['{0} spell', 'the {0} spell']),
-  createClass: dir => createType(dir, 'classes.json', 'Class', ['{0} class', 'the {0} class']),
-  createRace: dir => createType(dir, 'races.json', 'Race', ['{0} race', 'the {0} race']),
-  createMagicItem: dir => createType(dir, 'magic-items.json', 'MagicItem', ['the {0}']),
-  createEquipment: dir => createType(dir, 'equipment.json', 'Equipment', ['the {0}']),
-  createBackground: dir => createType(dir, 'backgrounds.json', 'Background', ['{0} background', 'the {0} background']),
-  createFeat: dir => createType(dir, 'feats.json', 'Feat', ['the {0} feat', '{0} feat']),
-  createMonsters: dir => createType(dir, 'monsters.json', 'Monster', ['the {0}', 'the {0} creature', 'the {0} monster', 'the {0} stats']),
+  createSpell: dir => createType(dir, 'spells.json', 'Spell'),
+  createClass: dir => createType(dir, 'classes.json', 'Class'),
+  createRace: dir => createType(dir, 'races.json', 'Race'),
+  createMagicItem: dir => createType(dir, 'magic-items.json', 'MagicItem'),
+  createEquipment: dir => createType(dir, 'equipment.json', 'Equipment'),
+  createBackground: dir => createType(dir, 'backgrounds.json', 'Background'),
+  createFeat: dir => createType(dir, 'feats.json', 'Feat'),
+  createMonsters: dir => createType(dir, 'monsters.json', 'Monster'),
   createSubclass,
   createSubrace
 };
